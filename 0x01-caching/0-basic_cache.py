30830ce9e18dd9815ab1fcf6670753e579da8d0e
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-21 19:47:26 +0000

    Add 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Basic_Cache
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Implements a basic cache"""
+
+    def put(self, key, item):
+        """Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key
+        """
+        return self.cache_data.get(key, None)
