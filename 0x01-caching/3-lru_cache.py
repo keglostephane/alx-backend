ab655210a0cfa3522e11b39f6d01e7386c9a4ed3
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-23 05:05:32 +0000

    Add 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""LRU_Cache
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Implements a LRU cache"""
+
+    def __init__(self):
+        """Initializer"""
+        super().__init__()
+        # list of keys from least recent to most recent
+        self.recently_used = []
+
+    def put(self, key, item):
+        """Add an item in the cache
+        """
+        if key and item:
+            if key in self.recently_used:
+                self.recently_used.remove(key)
+            elif len(self.cache_data) >= super().MAX_ITEMS:
+                discard = self.recently_used.pop(0)
+                self.cache_data.pop(discard)
+                print(f"DISCARD: {discard}")
+
+            self.cache_data[key] = item
+            self.recently_used.append(key)
+
+    def get(self, key):
+        """Get an item by key
+        """
+        if key in self.recently_used:
+            self.recently_used.remove(key)
+            self.recently_used.append(key)
+        return self.cache_data.get(key, None)
